c26340886bcf1691877cd5fbc5fc6cb823d17b49
xteam jack <xteam.jack@gmail.com>
2024-11-30 16:18:30 +0530

    public actions: advice api added

---
+++ actions/public.js
@@ -0,0 +1,20 @@
+import { unstable_cache } from "next/cache";
+
+export const get_daily_prompt = unstable_cache(
+  async () => {
+    try {
+      const res = await fetch("https://api.adviceslip.com/advice", {
+        cache: "no-store",
+      });
+      const data = await res.json();
+      return data.slip.advice;
+    } catch (error) {
+      return "What's on your mind?";
+    }
+  },
+  ["daily-prompt"],
+  {
+    revalidate: 1,
+    tags: ["daily-prompt"],
+  }
+);
--- app/page.js
+++ app/page.js
@@ -1,15 +1,18 @@
 import { Button } from "@/components/ui/button";
-import { buttonVariants } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button";
 import { Link } from "lucide-react";
+import { get_daily_prompt } from "@/actions/public";
 
-export default function Home() {
+export default async function Home() {
+  const advice = await get_daily_prompt();
   return (
     <div>
       <h1 className="text-3xl font-bold capitalize">Hello world!</h1>
       <Button variant="primary">Button!</Button>
-      <br/>
-      <hr/>
+      <br />
+      <hr />
       <Link className={buttonVariants({ variant: "primary" })}>Click here</Link>
+      <div>{advice}</div>
     </div>
   );
 }
