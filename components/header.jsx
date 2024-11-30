8823fd15839978bd2387ad915cef6b811b907d10
xteam jack <xteam.jack@gmail.com>
2024-11-30 21:45:51 +0530

    Basic prisma functionality added

--- components/header.jsx
+++ components/header.jsx
@@ -1,28 +1,27 @@
-import { SignedIn, UserButton } from '@clerk/nextjs'
-import { SignedOut, SignInButton } from '@clerk/nextjs'
-import Link from 'next/link'
-import React from 'react'
+import { checkUser } from "@/lib/checkUser";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedOut, SignInButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
+  checkUser();
   return (
     <header>
-        <nav className='py-6 px-4 flex justify-between items-center'>
-            <Link href={"/"}>
-                DigiHire
-            </Link>
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href={"/"}>DigiHire</Link>
 
-            <div className='flex items-center gap-4'>
-                <SignedOut>
-                    <SignInButton />
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-
-            </div>
-        </nav>
+        <div className="flex items-center gap-4">
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
